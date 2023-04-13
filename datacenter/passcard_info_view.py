f46534fa55b68d8c56e9c68ee54bc0f930cda715
Konstantin Fedosov <lkfed@mail.ru>
2023-04-13 22:51:24 +0300

    added some awesome features

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,19 +1,33 @@
-from datacenter.models import Passcard
-from datacenter.models import Visit
-from django.shortcuts import render
+from datetime import timedelta
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from datacenter.models import Passcard, Visit
+from datacenter.storage_information_view import get_duration, format_duration
+
+LONG_VISIT_MINUTES = 60
+
+
+def is_visit_long(visit, minutes=LONG_VISIT_MINUTES):
+    duration = get_duration(visit)
+    return duration > timedelta(minutes=minutes)
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
+    passcard = get_object_or_404(Passcard, passcode=passcode)
 
-    this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
-    ]
+    visits = Visit.objects.filter(passcard=passcard).order_by('-entered_at')
+
+    this_passcard_visits = []
+    for visit in visits:
+        duration = get_duration(visit)
+        is_strange = is_visit_long(visit)
+
+        this_passcard_visits.append({
+            'entered_at': visit.entered_at,
+            'duration': duration,
+            'is_strange': is_strange
+        })
+
     context = {
         'passcard': passcard,
         'this_passcard_visits': this_passcard_visits
